f7ba451c7a8a39ba1dacbab365b89366dd93ba5f
愚木头 <2676171993@qq.com>
2023-11-03 15:23:42 +0800

    新版本上传

--- docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
-import './index.css'
-
-export default {
-    ...DefaultTheme,
-    Layout,
-    enhanceApp({ app }) {},
-}
